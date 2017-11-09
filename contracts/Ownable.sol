c3de3348531c50c7c2c2d9aecb24694e80d5b6ce
Kostas Kouvaris <k_kouvaris@hotmail.com>
2017-11-09 16:20:07 +0000

    Adds a function kill() that can be used to deactivate the respective owned contract, returning all potential funds to the owner's address. Adds a function passOnwership() that can be used to pass the ownership of the given contract other users, assigning their address to "owner" variablie. Adds a getter method to retrieve the address of the current owner of the respective owned contract.

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -7,6 +7,22 @@ contract Ownable {
     address internal owner;
 
     /** @dev Constructor: instantiates onwership for derived contracts */
-    function Ownable() public { owner = msg.sender; }
+    function Ownable() public {owner = msg.sender;}
+
+    /** @dev passes the ownership of the current contract to another address. Only the current owner can call this function.
+        @param _newOwner The address of the new owner. 
+        */
+    function passOwnership(address _newOwner) internal { 
+        require(_newOwner != 0x0);
+        owner = _newOwner; 
+    }
+
+    /** @dev deactivates the current contract and returns all remaining ethers to the owner's address. Only the current owner can call this function. */
+    function kill() internal {selfdestruct(owner);}
+
+    /** @dev retrieves the address of the current owner.
+        @return The address of the current owner. 
+        */
+    function getOwner() internal constant returns (address) {return owner;}    
 
 }
